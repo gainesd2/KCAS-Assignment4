2145bf09b26a63b2ebd2f9cac730b7aef7950211
QuentinRoa1 <98721340+QuentinRoa1@users.noreply.github.com>
2022-02-28 01:26:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ let dogs =[
     {name:"Quarterback", id:24142,sex:"Male",age:"3Yrs 6Mths",status:"Spayed/Neutered",breed:"Boxer mixed Mastiff",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71541_th_600.jpg"},
 
 ]
+function dog(){
+    for (dog in dogs){
+        print("<div class=\"dog\">");
+        print("<img class=\"dogimg\"",dog.pic,">");
+        print("<h3 class=\"dogname\">",dog.name,"</h3>");
+        print("<p class=\"dogbreed\">Breed: ",dog.breed,"</p>");
+        print("<p class=\"dogid\">ID: ",dog.id,"</p>");
+        print("<p class=\"dogsex\">Sex: ",dog.sex,"</p>");
+        print("<p class=\"dogstatus\"Status>",dog.status,"</p>");
+        print("<p class=\"dogage\">Age: ",dog.age,"</p>");
+        print("</div>");
+    }
+}
+function nextpage(){
+    let i=1;
+    while(i<4){
+        print("<a class=\"page\"href=\"/?page=",i-1,"\">",i,"</a>");
+    }
+    print("<a class=\"page\" href\","i-1,"\"\\>\\></a>");
+}
