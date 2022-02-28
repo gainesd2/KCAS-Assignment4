d956c6502a63436a218fd92d57615defe59c4481
QuentinRoa1 <98721340+QuentinRoa1@users.noreply.github.com>
2022-02-27 22:48:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let dogs =[
+    {name:"Pepper", id:23328,sex:"Female",age:"2Yrs 3Mths 1Wks",status:"Spayed/Neutered",breed:"American Pit Bull cross",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71168_th_600.jpeg"},
+    {name:"Hattie", id:23989,sex:"Female",age:"1Yrs 6Mths 2Wks",status:"Spayed/Neutered",breed:"American Pit Bull cross",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71438_th_600.jpg"},
+    {name:"Jimmy", id:24104,sex:"Male",age:"9Mths",status:"Spayed/Neutered",breed:"American Pit Bull cross",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71509_th_600.jpeg"},
+    {name:"Freya", id:23867,sex:"Female",age:"1Yrs 7Mths 1Wks",status:"Spayed/Neutered",breed:"Blue Heeler Cross",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71366_th_600.jpeg"},
+    {name:"Kimbo", id:23879,sex:"Male",age:"1Yrs 7Mths 1Wks",status:"Spayed/Neutered",breed:"Boxer mixed Mastiff",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71369_th_600.jpg"},
+    {name:"Quarterback", id:24142,sex:"Male",age:"3Yrs 6Mths",status:"Spayed/Neutered",breed:"Boxer mixed Mastiff",pic:"http://kentoncountypets.shelterbuddy.com/photos/lostfound/71541_th_600.jpg"},
+
+]
