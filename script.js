d8e954c0ddeb2c272ef1fb4092c8f780c0ec332f
Dylan <99437817+gainesd2@users.noreply.github.com>
2022-03-10 13:03:47 -0500

    Polished Cards
    Got the cards to look better, not done, but need to run, so pushing this before I go

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict"
 
 const dogs = [
-    { Name: "Pepper", ID: 23328, Sex: "Female", Age: "2Yrs 3Mths 1Wks", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71168_th_600.jpeg" },
-    { Name: "Hattie", ID: 23989, Sex: "Female", Age: "1Yrs 6Mths 2Wks", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71438_th_600.jpg" },
-    { Name: "Jimmy", ID: 24104, Sex: "Male", Age: "9Mths", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71509_th_600.jpeg" },
-    { Name: "Freya", ID: 23867, Sex: "Female", Age: "1Yrs 7Mths 1Wks", Status: "Spayed/Neutered", Breed: "Blue Heeler Cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71366_th_600.jpeg" },
-    { Name: "Kimbo", ID: 23879, Sex: "Male", Age: "1Yrs 7Mths 1Wks", Status: "Spayed/Neutered", Breed: "Boxer mixed Mastiff", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71369_th_600.jpg" },
-    { Name: "Quarterback", ID: 24142, Sex: "Male", Age: "3Yrs 6Mths", Status: "Spayed/Neutered", Breed: "Boxer mixed Mastiff", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71541_th_600.jpg" },
+    { Name: "Pepper", ID: 23328, Sex: "female", Age: "2Yrs 3Mths 1Wks", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71168_th_600.jpeg" },
+    { Name: "Hattie", ID: 23989, Sex: "female", Age: "1Yrs 6Mths 2Wks", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71438_th_600.jpg" },
+    { Name: "Jimmy", ID: 24104, Sex: "male", Age: "9Mths", Status: "Spayed/Neutered", Breed: "American Pit Bull cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71509_th_600.jpeg" },
+    { Name: "Freya", ID: 23867, Sex: "female", Age: "1Yrs 7Mths 1Wks", Status: "Spayed/Neutered", Breed: "Blue Heeler Cross", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71366_th_600.jpeg" },
+    { Name: "Kimbo", ID: 23879, Sex: "male", Age: "1Yrs 7Mths 1Wks", Status: "Spayed/Neutered", Breed: "Boxer mixed Mastiff", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71369_th_600.jpg" },
+    { Name: "Quarterback", ID: 24142, Sex: "male", Age: "3Yrs 6Mths", Status: "Spayed/Neutered", Breed: "Boxer mixed Mastiff", Img: "http://kentoncountypets.shelterbuddy.com/photos/lostfound/71541_th_600.jpg" },
 ]
 
 
@@ -16,15 +16,17 @@ function dog() {
         Card: document.createElement("div"),
         Img: document.createElement("img"),
         Name: document.createElement("h3"),
-        Breed: document.createElement("p"),
-        ID: document.createElement("p"),
-        Sex: document.createElement("p"),
+        Breed: document.createElement("h4"),
+        ID: document.createElement("small"),
+        Sex: document.createElement("span"),
         Status: document.createElement("p"),
         Age: document.createElement("p")
     }
 
     // create the template card
     let dogTemplate = cardTemplater("dog", CARDPARTS, ".main-cardHolder");
+
+    dogDeck.classList.add()
 
     // use the template to make the needed cards
     cardLoader("dog", dogTemplate, dogDeck, dogs);
@@ -55,18 +57,48 @@ function cardTemplater(baseName, CARDPARTS, dest) {
 
         // adds class name to current part
         CARDPARTS[part].classList.add(className);
-
-        // append all card parts to template card node
-        // skip "card" because you can't append the card to itself
-        if (part == "Card") {
-            continue;
-        } else {
-            CARDPARTS["Card"].appendChild(CARDPARTS[part]);
-        }
     }
 
-    // append template card to deck
-    deck.appendChild(CARDPARTS["Card"]);
+    let cardClasses = "card";
+    let imgClasses = "card-img-top";
+    let IDClasses = "";
+    let nameClasses = "card-title";
+    let sexClasses = "badge bi rounded-pill ms-2";
+    let breedClasses = "card-subtitle text-muted";
+    let ageClasses = "";
+    let statusClasses = "";
+
+    // add card-body
+    let cardBody = document.createElement("div");
+    let cardBodyClasses = "card-body"
+
+    // card
+    // add card-img-top
+    cardFormatter(CARDPARTS["Img"], CARDPARTS["Card"], imgClasses);
+
+    // add ID to cardBody
+    cardFormatter(CARDPARTS["ID"], cardBody, IDClasses);
+
+    // Format name and add it to cardBody
+    cardFormatter(CARDPARTS["Name"], cardBody, nameClasses);
+
+    // Format Sex and add it to Breed
+    cardFormatter(CARDPARTS["Sex"], CARDPARTS["Breed"], sexClasses);
+
+    // Format Breed and add it to cardBody
+    cardFormatter(CARDPARTS["Breed"], cardBody, breedClasses);
+
+    // add age to cardBody
+    cardFormatter(CARDPARTS["Age"], cardBody, ageClasses);
+
+    // add status to cardBody
+    cardFormatter(CARDPARTS["Status"], cardBody, statusClasses);
+
+    // format cardBody and add it to the card
+    cardFormatter(cardBody, CARDPARTS["Card"], cardBodyClasses);
+
+    // add card template to the deck
+    cardFormatter(CARDPARTS["Card"], deck, cardClasses);
 
     // set ID for card Template
     CARDPARTS["Card"].setAttribute("id", baseName + "CardTemplate");
@@ -80,7 +112,7 @@ function cardTemplater(baseName, CARDPARTS, dest) {
 /**
  * Generates a card based on a provided list with custom class names. 
  * @param {string} baseName the prefix of all class names for card parts
- * @param {object} cardTemplate hidden card template element on page
+ * @param {HTMLElement} cardTemplate hidden card template element on page
  * @param {string} dest destination node a card is appeneded to, usually a deck
  * @param {array} data Array of objects used to populate cards.
  * @returns {string} string representing the ID of the template card
@@ -99,18 +131,25 @@ function cardLoader(baseName, cardTemplate, dest, data) {
         // set attributes of new Card:
         // > all data except image is appended to element. Images have source set
         for (let attribute in entry) {
-            // if image, set source
-            if (attribute == "Img") {
-                let imageHolder = newCard.querySelector(`.${baseName}Card-Img`);
-                imageHolder.setAttribute("src", entry.Img);
+            // if image, set source and add listener for 404 errors
+            // if sex, add proper notation
+            switch (attribute) {
+                case "Img":
+                    let imageHolder = newCard.querySelector(`.${baseName}Card-Img`);
+                    imageHolder.setAttribute("src", entry.Img);
 
-                // add an event listener that handles 404 errors for images
-                imageHolder.addEventListener("error", () => {
-                    imageHolder.setAttribute("src", placeholderImage("No Image Found"));
-                });
-                continue;
-            } else {
-                newCard.querySelector("." + baseName + "Card-" + attribute).append(entry[attribute]);
+                    // add an event listener that handles 404 errors for images
+                    imageHolder.addEventListener("error", () => {
+                        imageHolder.setAttribute("src", placeholderImage("No Image Found"));
+                    });
+                    break;
+                case "Sex":
+                    let sexBadge = newCard.querySelector("." + baseName + "Card-" + attribute);
+                    sexBadge.classList.add(`bi-gender-${entry[attribute]}`, `bg-${entry[attribute]}`);
+                    sexBadge.append(" ");
+                    break;
+                default:
+                    newCard.querySelector("." + baseName + "Card-" + attribute).prepend(entry[attribute]);
             }
         }
 
@@ -121,7 +160,7 @@ function cardLoader(baseName, cardTemplate, dest, data) {
         newCard.hidden = false;
 
         cardSleeve.appendChild(newCard);
-        cardFormatter(cardSleeve);
+        //cardFormatter(cardSleeve);
         dest.appendChild(cardSleeve);
     }
 }
@@ -140,36 +179,18 @@ function cardLoader(baseName, cardTemplate, dest, data) {
  */
 
 /**
- * Adds bootstrap classes to generated cards
- * @param {HTMLAnchorElement} cardSleeve link to the <a> tag element holding a card
+ * adds classes from class list string to the card part and appends it to the destination
+ * @param {HTMLElement} cardPart The part to be formatted
+ * @param {HTMLElement} destination The destination HTML element
+ * @param {string} classList A space-seperated string of class names. Formatted like an html classlist "class1 class2 etc"
  */
-function cardFormatter(cardSleeve) {
-    // add to card sleeve
-    cardSleeve.classList.add("d-flex");
+function cardFormatter(cardPart, destination, classList = "") {
+    if (classList !== "") {
+        classList = classList.split(" ");
+        cardPart.classList.add(...classList);
+    }
 
-    // dogCard
-    cardSleeve.querySelector(".dogCard").classList.add("card" /*, "flex-row"*/ );
-
-    // dogCard-Img
-    cardSleeve.querySelector(".dogCard-Img").classList.add();
-
-    // dogCard-Name
-    cardSleeve.querySelector(".dogCard-Name").classList.add();
-
-    // dogCard-Breed
-    cardSleeve.querySelector(".dogCard-Breed").classList.add();
-
-    // dogCard-ID
-    cardSleeve.querySelector(".dogCard-ID").classList.add();
-
-    // dogCard-Sex
-    cardSleeve.querySelector(".dogCard-Sex").classList.add();
-
-    // dogCard-Status
-    cardSleeve.querySelector(".dogCard-Status").classList.add();
-
-    // dogCard-Age
-    cardSleeve.querySelector(".dogCard-Age").classList.add();
+    destination.appendChild(cardPart);
 }
 
 /**
